0f656c15855b7719262eb008b24bd57937d4e769
Badr.Gharib <Badr.Gharib@QCENTRIS86>
2019-06-13 00:08:19 +0200

    nav bar done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {signOut} from '../actions/authedUser'
 
 class Nav extends React.Component{
+    handelSignOut=(e)=>{
+        const {dispatch}=this.props
+        dispatch(signOut())
+    }
     render(){
+        debugger;
+        const{user}=this.props
         return (
            <nav className='nav'>
             <ul>
@@ -13,8 +21,28 @@ class Nav extends React.Component{
                     <NavLink to='/question' activeClassName='active'>New Question</NavLink>
                 </li>
             </ul>
+           
+            
+            {(user!==null)&&
+                <div className='autheduserInfo'>
+                   <span>{`Hello, ${user.name}`}</span>
+                    <img src={user.avatarURL}
+                    alt={`Avatar of ${user.name}`}
+                    className='avatar'/>
+                    <NavLink to='/' activeClassName='active' onClick={this.handelSignOut}>Sign Out</NavLink>
+                </div>
+            }
+            
+             
+           
            </nav>
         )
     }
 }
-export default Nav
+function mapStateToProps({authedUser,users}){
+   // debugger;
+ return {
+     user:authedUser===null?null:users[authedUser]
+ }
+}
+export default connect(mapStateToProps)(Nav)
--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -9,12 +9,14 @@ class SignInPage extends React.Component{
         userId:''
     }
     handelChange=(e)=>{
+        debugger;
         const userId=e.target.value
         this.setState(()=>({
             userId
         }))
     }
     handelSubmit=(e)=>{
+        debugger;
         e.preventDefault()
         const {userId}=this.state
         const {dispatch}=this.props
@@ -41,9 +43,10 @@ class SignInPage extends React.Component{
               <img src={logo} className="App-logo" alt="logo" />
               <span className='center signIn-label'>Sign in</span>
               <form onSubmit={this.handelSubmit} className='column-center'>
-                <select value={this.state.value} handelChange={this.handelChange} className='signIn-select'>
+                <select  value={this.state.value} onChange={this.handelChange} className='signIn-select'>
+                <option disabled selected value> -- select User -- </option>
                     {
-                    users.map((user)=>(<option key={user.id}>{user.name}</option>))
+                    users.map((user)=>(<option key={user.id} value={user.id}>{user.name}</option>))
                     }
                 </select>
 
@@ -57,7 +60,7 @@ class SignInPage extends React.Component{
 }
 
 function mapStateToProps({users}){
-    debugger;
+   // debugger;
   return {
       users:Object.values(users)
   }
